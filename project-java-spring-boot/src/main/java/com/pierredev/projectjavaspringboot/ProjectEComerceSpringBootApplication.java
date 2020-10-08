19b571cbdbb41d9e19e526ea38470cec733f167b
pierreEnoc <pierre_enoc@yahoo.fr>
2020-10-08 20:52:20 -0300

    :sparkles: start project ecomerce

---
+++ project-java-spring-boot/src/main/java/com/pierredev/projectjavaspringboot/ProjectEComerceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pierredev.projectjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEComerceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEComerceSpringBootApplication.class, args);
+	}
+
+}
---
+++ project-java-spring-boot/src/test/java/com/pierredev/projectjavaspringboot/ProjectEComerceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pierredev.projectjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEComerceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
